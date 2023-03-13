65825def6c4a8fa5aff69797fa5d4d6458504e4a
wami <wamiq@brandver.se>
2023-03-13 10:39:13 +0500

    comments

--- lib/component.dart
+++ lib/component.dart
@@ -57,160 +57,148 @@ class _CreditCardFormState extends State<CreditCardForm> {
   @override
   Widget build(BuildContext context) {
     CreditCardTheme theme = widget.theme ?? CreditCardLightTheme();
-    return Container(
-      // decoration: BoxDecoration(
-      //   color: theme.backgroundColor,
-      //   border: Border.all(color: theme.borderColor, width: 1),
-      //   borderRadius: const BorderRadius.only(
-      //     topLeft: Radius.circular(10),
-      //     topRight: Radius.circular(10),
-      //     bottomLeft: Radius.circular(10),
-      //     bottomRight: Radius.circular(10),
-      //   ),
-      // ),
-      child: Form(
-        key: widget.formKey,
-        child: Column(
-          children: [
-            textInput(
-              controller: controllers['card'],
-              label: widget.cardNumberLabel ?? 'Card Number',
-              validator: (value) {
-                String? validateMsg = cardNumberValidator(value);
-                validateMsg == null
-                    ? isCardNumberValidated = true
-                    : isCardNumberValidated = false;
-                setState(() {});
-                return validateMsg;
-              },
-              key: 'card',
-              bottom: 1,
-              formatters: [
-                FilteringTextInputFormatter.digitsOnly,
-                LengthLimitingTextInputFormatter(16),
-                CardNumberInputFormatter(),
-              ],
-              onChanged: (val) {
-                Map img = CardUtils.getCardIcon(val);
-                CardType type =
-                    CardUtils.getCardTypeFrmNumber(val.replaceAll(' ', ''));
-                setState(() {
-                  cardImg = img;
-                  cardType = type;
-                });
-                emitResult();
-              },
-              suffixIcon: isCardNumberValidated == null
-                  ? const SizedBox()
-                  : isCardNumberValidated!
-                      ? const Icon(
-                          Icons.check_circle,
-                          color: Colors.green,
-                        )
-                      : const Icon(Icons.info_outline),
-              prefixIcon: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Image.asset(
-                  'images/${cardImg['img']}',
-                  package: 'credit_card_form',
-                  width: cardImg['width'] as double?,
-                ),
+    return Form(
+      key: widget.formKey,
+      child: Column(
+        children: [
+          textInput(
+            controller: controllers['card'],
+            label: widget.cardNumberLabel ?? 'Card Number',
+            validator: (value) {
+              String? validateMsg = cardNumberValidator(value);
+              validateMsg == null
+                  ? isCardNumberValidated = true
+                  : isCardNumberValidated = false;
+              setState(() {});
+              return validateMsg;
+            },
+            key: 'card',
+            bottom: 1,
+            formatters: [
+              FilteringTextInputFormatter.digitsOnly,
+              LengthLimitingTextInputFormatter(16),
+              CardNumberInputFormatter(),
+            ],
+            onChanged: (val) {
+              Map img = CardUtils.getCardIcon(val);
+              CardType type =
+                  CardUtils.getCardTypeFrmNumber(val.replaceAll(' ', ''));
+              setState(() {
+                cardImg = img;
+                cardType = type;
+              });
+              emitResult();
+            },
+            suffixIcon: isCardNumberValidated == null
+                ? const SizedBox()
+                : isCardNumberValidated!
+                    ? const Icon(
+                        Icons.check_circle,
+                        color: Colors.green,
+                      )
+                    : const Icon(Icons.info_outline),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Image.asset(
+                'images/${cardImg['img']}',
+                package: 'credit_card_form',
+                width: cardImg['width'] as double?,
               ),
             ),
-            const SizedBox(height: 12),
-            textInput(
-              label: widget.cardHolderLabel ?? 'Card holder name',
-              controller: controllers['card_holder_name'],
-              key: 'card_holder_name',
-              validator: (value) => emptyValidator(value, 'Account Title'),
-              bottom: 1,
-              onChanged: (val) {
-                emitResult();
-              },
-              keyboardType: TextInputType.name,
-            ),
-            const SizedBox(height: 12),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Flexible(
-                  flex: 4,
-                  child: textInput(
-                    label: widget.expiredDateLabel ?? 'MM/YY',
-                    hintText: 'MM / YY',
-                    right: 1,
-                    validator: (value) {
-                      String? validateMsg = cardExpiryDateValidator(value);
-                      validateMsg == null
-                          ? isExpiryValidated = true
-                          : isExpiryValidated = false;
-                      setState(() {});
-                      return validateMsg;
-                    },
-                    key: 'expired_date',
+          ),
+          const SizedBox(height: 12),
+          textInput(
+            label: widget.cardHolderLabel ?? 'Card holder name',
+            controller: controllers['card_holder_name'],
+            key: 'card_holder_name',
+            validator: (value) => emptyValidator(value, 'Account Title'),
+            bottom: 1,
+            onChanged: (val) {
+              emitResult();
+            },
+            keyboardType: TextInputType.name,
+          ),
+          const SizedBox(height: 12),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                flex: 4,
+                child: textInput(
+                  label: widget.expiredDateLabel ?? 'MM/YY',
+                  hintText: 'MM / YY',
+                  right: 1,
+                  validator: (value) {
+                    String? validateMsg = cardExpiryDateValidator(value);
+                    validateMsg == null
+                        ? isExpiryValidated = true
+                        : isExpiryValidated = false;
+                    setState(() {});
+                    return validateMsg;
+                  },
+                  key: 'expired_date',
+                  onChanged: (val) {
+                    emitResult();
+                  },
+                  suffixIcon: isExpiryValidated == null
+                      ? const SizedBox()
+                      : isExpiryValidated!
+                          ? const Icon(
+                              Icons.check_circle,
+                              color: Colors.green,
+                            )
+                          : const Icon(Icons.info_outline),
+                  controller: controllers['expired_date'],
+                  formatters: [
+                    CardExpirationFormatter(),
+                    LengthLimitingTextInputFormatter(5)
+                  ],
+                ),
+              ),
+              const SizedBox(width: 12),
+              Flexible(
+                flex: 4,
+                child: textInput(
+                    label: widget.cvcLabel ?? 'CVC',
+                    hintText: '***',
+                    key: 'cvc',
+                    validator: cvvValidator,
+                    controller: controllers['cvc'],
+                    password: true,
                     onChanged: (val) {
                       emitResult();
                     },
-                    suffixIcon: isExpiryValidated == null
-                        ? const SizedBox()
-                        : isExpiryValidated!
-                            ? const Icon(
-                                Icons.check_circle,
-                                color: Colors.green,
-                              )
-                            : const Icon(Icons.info_outline),
-                    controller: controllers['expired_date'],
                     formatters: [
-                      CardExpirationFormatter(),
-                      LengthLimitingTextInputFormatter(5)
+                      FilteringTextInputFormatter.digitsOnly,
+                      LengthLimitingTextInputFormatter(3)
                     ],
-                  ),
-                ),
-                const SizedBox(width: 12),
-                Flexible(
-                  flex: 4,
-                  child: textInput(
-                      label: widget.cvcLabel ?? 'CVC',
-                      hintText: '***',
-                      key: 'cvc',
-                      validator: cvvValidator,
-                      controller: controllers['cvc'],
-                      password: true,
-                      onChanged: (val) {
-                        emitResult();
-                      },
-                      formatters: [
-                        FilteringTextInputFormatter.digitsOnly,
-                        LengthLimitingTextInputFormatter(3)
-                      ],
-                      suffixIcon: Icon(
-                        Icons.help,
-                        color: theme.borderColor,
-                      )),
-                )
-              ],
+                    suffixIcon: Icon(
+                      Icons.help,
+                      color: theme.borderColor,
+                    )),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 12, bottom: 8),
+            child: textInput(
+              label: 'Add a card nickname',
+              key: 'nickname',
+              controller: controllers['nickname'],
+              validator: (value) => emptyValidator(value, 'Card Nickname'),
+              onChanged: (val) => emitResult(),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 12, bottom: 8),
-              child: textInput(
-                label: 'Add a card nickname',
-                key: 'nickname',
-                controller: controllers['nickname'],
-                validator: (value) => emptyValidator(value, 'Card Nickname'),
-                onChanged: (val) => emitResult(),
-              ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Example: My Gold Card',
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  color: Theme.of(context).hintColor,
+                  fontWeight: FontWeight.w600),
             ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Example: My Gold Card',
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    color: Theme.of(context).hintColor,
-                    fontWeight: FontWeight.w600),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
